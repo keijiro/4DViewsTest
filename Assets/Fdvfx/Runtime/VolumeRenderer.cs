625b4af34f7d62878ac4eb8bcbead87fbe9364bf
Keijiro Takahashi <keijiro@gmail.com>
2020-01-29 21:20:47 +0900

    Refactoring

--- Assets/Fdvfx/Runtime/VolumeRenderer.cs
+++ Assets/Fdvfx/Runtime/VolumeRenderer.cs
@@ -34,26 +34,31 @@ namespace Fdvfx
 
         #region Internal-use objects
 
+        // 4DViews data source and controller variables
         DataSource4DS _dataSource;
         int _totalFrames;
         float _frameRate;
         int _lastFrame = -1;
 
+        // Buffers used to receive data from the 4DViews data source
         (NativeArray<Vector3> vertex,
          NativeArray<Vector3> normal,
          NativeArray<Vector2> uv,
          NativeArray<int> index,
          NativeArray<byte> texture) _sourceBuffer;
 
+        // Objects for surface rendering
         Mesh _mesh;
         Texture2D _texture;
         MaterialPropertyBlock _overrides;
 
+        // Buffers used to bake the point cloud
         (ComputeBuffer vertex,
          ComputeBuffer normal,
          ComputeBuffer uv,
          ComputeBuffer index) _bakeBuffer;
 
+        // Objects for point cloud baking
         RenderBuffer[] _mrt = new RenderBuffer[2];
         Material _bakeMaterial;
 
@@ -95,6 +100,8 @@ namespace Fdvfx
 
         void OnDisable()
         {
+            // We have to release these buffers in OnDisable, not in OnDestroy.
+
             if (_sourceBuffer.vertex .IsCreated) _sourceBuffer.vertex .Dispose();
             if (_sourceBuffer.normal .IsCreated) _sourceBuffer.normal .Dispose();
             if (_sourceBuffer.uv     .IsCreated) _sourceBuffer.uv     .Dispose();
@@ -121,7 +128,7 @@ namespace Fdvfx
             DestroySafely(ref _bakeMaterial);
         }
 
-        unsafe void Update()
+        void Update()
         {
             // Data source lazy initialization
             if (_dataSource == null)
@@ -133,11 +140,9 @@ namespace Fdvfx
 
                 _totalFrames = Bridge4DS.GetSequenceNbFrames(_dataSource.FDVUUID);
                 _frameRate = Bridge4DS.GetSequenceFramerate(_dataSource.FDVUUID);
-
-                Bridge4DS.SetSpeed(_dataSource.FDVUUID, 0);
-                Bridge4DS.Play(_dataSource.FDVUUID, true);
             }
 
+            // Source buffer lazy initialization
             if (!_sourceBuffer.vertex.IsCreated)
             {
                 var vcount = _dataSource.MaxVertices;
@@ -146,32 +151,20 @@ namespace Fdvfx
                 var texsize = _dataSource.TextureSize;
                 texsize = texsize * texsize / 2; 
 
-                _sourceBuffer.vertex  = new NativeArray<Vector3>( vcount, Allocator.Persistent);
-                _sourceBuffer.normal  = new NativeArray<Vector3>( vcount, Allocator.Persistent);
-                _sourceBuffer.uv      = new NativeArray<Vector2>( vcount, Allocator.Persistent);
-                _sourceBuffer.index   = new NativeArray<    int>( icount, Allocator.Persistent);
-                _sourceBuffer.texture = new NativeArray<   byte>(texsize, Allocator.Persistent);
+                _sourceBuffer = (
+                    vertex  : new NativeArray<Vector3>( vcount, Allocator.Persistent),
+                    normal  : new NativeArray<Vector3>( vcount, Allocator.Persistent),
+                    uv      : new NativeArray<Vector2>( vcount, Allocator.Persistent),
+                    index   : new NativeArray<    int>( icount, Allocator.Persistent),
+                    texture : new NativeArray<   byte>(texsize, Allocator.Persistent)
+                );
             }
 
-            if (_bakeBuffer.vertex == null)
-            {
-                var vcount = _dataSource.MaxVertices;
-                var tcount = _dataSource.MaxTriangles;
-
-                _bakeBuffer.vertex  = new ComputeBuffer(vcount * 3, sizeof(float));
-                _bakeBuffer.normal  = new ComputeBuffer(vcount * 3, sizeof(float));
-                _bakeBuffer.uv      = new ComputeBuffer(vcount * 2, sizeof(float));
-                _bakeBuffer.index   = new ComputeBuffer(tcount * 3, sizeof(int));
-            }
-
+            // Mesh objects lazy initialization
             if (_mesh == null)
-            {
-                _mesh = new Mesh();
-                _mesh.hideFlags = HideFlags.DontSave;
-            }
+                _mesh = new Mesh() { hideFlags = HideFlags.DontSave };
 
             if (_texture == null)
-            {
                 _texture = new Texture2D(
                     _dataSource.TextureSize,
                     _dataSource.TextureSize,
@@ -182,68 +175,97 @@ namespace Fdvfx
                     filterMode = FilterMode.Bilinear,
                     hideFlags = HideFlags.DontSave
                 };
-            }
 
-            if (_overrides == null) _overrides = new MaterialPropertyBlock();
+            if (_overrides == null)
+                _overrides = new MaterialPropertyBlock();
 
-            if (_bakeMaterial == null)
-                _bakeMaterial =
-                    new Material(_bakeShader){ hideFlags = HideFlags.DontSave };
-
-            if (_dataSource != null)
+            // Point cloud bake buffer lazy initialization
+            if (_bakeBuffer.vertex == null)
             {
-                var frame = Mathf.Clamp((int)(_time * _frameRate), 0, _totalFrames - 1);
+                var vcount = _dataSource.MaxVertices;
+                var tcount = _dataSource.MaxTriangles;
 
-                if (frame != _lastFrame)
-                {
-                    Bridge4DS.GotoFrame(_dataSource.FDVUUID, frame);
-
-                    int vertexCount = 0, indexCount = 0;
-
-                    var modelID = Bridge4DS.UpdateModel(
-                        _dataSource.FDVUUID,
-                        (IntPtr)NativeArrayUnsafeUtility.GetUnsafePtr(_sourceBuffer.vertex),
-                        (IntPtr)NativeArrayUnsafeUtility.GetUnsafePtr(_sourceBuffer.uv),
-                        (IntPtr)NativeArrayUnsafeUtility.GetUnsafePtr(_sourceBuffer.index),
-                        (IntPtr)NativeArrayUnsafeUtility.GetUnsafePtr(_sourceBuffer.texture),
-                        (IntPtr)NativeArrayUnsafeUtility.GetUnsafePtr(_sourceBuffer.normal),
-                        _lastFrame, ref vertexCount, ref indexCount
-                    );
-
-                    _bakeMaterial.SetInt("_VertexCount", vertexCount);
-
-                    _mesh.SetVertices(_sourceBuffer.vertex);
-                    _mesh.SetNormals(_sourceBuffer.normal);
-                    _mesh.SetUVs(0, _sourceBuffer.uv);
-                    _mesh.SetIndices(_sourceBuffer.index, MeshTopology.Triangles, 0);
-
-                    _texture.LoadRawTextureData(_sourceBuffer.texture);
-                    _texture.Apply();
-
-                    _bakeBuffer.vertex.SetData(_sourceBuffer.vertex);
-                    _bakeBuffer.uv.SetData(_sourceBuffer.uv);
-
-                    _lastFrame = frame;
-                }
+                _bakeBuffer = (
+                    vertex : new ComputeBuffer(vcount * 3, sizeof(float)),
+                    normal : new ComputeBuffer(vcount * 3, sizeof(float)),
+                    uv     : new ComputeBuffer(vcount * 2, sizeof(float)),
+                    index  : new ComputeBuffer(tcount * 3, sizeof(  int))
+                );
             }
 
-            _overrides.SetTexture("_MainTex", _texture);
+            // Point cloud bake shader (material) initialization
+            if (_bakeMaterial == null)
+                _bakeMaterial = new Material(_bakeShader){ hideFlags = HideFlags.DontSave };
 
-            _bakeMaterial.SetVector("_TextureSize", new Vector2(_positionMap.width, _positionMap.height));
+            // Try receiving a new frame from the data source.
+            var frame = Mathf.Clamp((int)(_time * _frameRate), 0, _totalFrames - 1);
+            if (frame != _lastFrame) ReceiveNewFrame(frame);
+
+            // Draw the surface mesh.
+            if (_surfaceMaterial != null)
+            {
+                _overrides.SetTexture("_MainTex", _texture);
+
+                Graphics.DrawMesh(
+                    _mesh, transform.localToWorldMatrix,
+                    _surfaceMaterial, gameObject.layer,
+                    null, 0, _overrides
+                );
+            }
+        }
+
+        #endregion
+
+        #region Data transfer method
+
+        unsafe void ReceiveNewFrame(int frame)
+        {
+            // Receive the mesh from the 4DViews data source.
+            Bridge4DS.GotoFrame(_dataSource.FDVUUID, frame);
+
+            int vertexCount = 0, indexCount = 0;
+
+            var modelID = Bridge4DS.UpdateModel(
+                _dataSource.FDVUUID,
+                (IntPtr)NativeArrayUnsafeUtility.GetUnsafePtr(_sourceBuffer.vertex),
+                (IntPtr)NativeArrayUnsafeUtility.GetUnsafePtr(_sourceBuffer.uv),
+                (IntPtr)NativeArrayUnsafeUtility.GetUnsafePtr(_sourceBuffer.index),
+                (IntPtr)NativeArrayUnsafeUtility.GetUnsafePtr(_sourceBuffer.texture),
+                (IntPtr)NativeArrayUnsafeUtility.GetUnsafePtr(_sourceBuffer.normal),
+                _lastFrame, ref vertexCount, ref indexCount
+            );
+
+            // Mesh update
+            _mesh.SetVertices(_sourceBuffer.vertex);
+            _mesh.SetNormals(_sourceBuffer.normal);
+            _mesh.SetUVs(0, _sourceBuffer.uv);
+            _mesh.SetIndices(_sourceBuffer.index, MeshTopology.Triangles, 0);
+
+            // Texture update
+            _texture.LoadRawTextureData(_sourceBuffer.texture);
+            _texture.Apply();
+
+            if (_colorMap != null) Graphics.Blit(_texture, _colorMap);
+
+            // Bake the mesh into the point cloud attribute maps
+            _bakeBuffer.vertex.SetData(_sourceBuffer.vertex);
             _bakeMaterial.SetBuffer("_VertexArray", _bakeBuffer.vertex);
+
+            _bakeBuffer.uv.SetData(_sourceBuffer.uv);
             _bakeMaterial.SetBuffer("_UVArray", _bakeBuffer.uv);
+
+            _bakeMaterial.SetInt("_VertexCount", vertexCount);
+            _bakeMaterial.SetVector("_TextureSize",
+                new Vector2(_positionMap.width, _positionMap.height));
 
             _mrt[0] = _positionMap.colorBuffer;
             _mrt[1] = _uvMap.colorBuffer;
             Graphics.SetRenderTarget(_mrt, _positionMap.depthBuffer);
-            Graphics.Blit(null, _bakeMaterial, 0);
-            Graphics.Blit(_texture, _colorMap);
 
-            if (_surfaceMaterial != null)
-                Graphics.DrawMesh(
-                    _mesh, transform.localToWorldMatrix,
-                    _surfaceMaterial, gameObject.layer, null, 0, _overrides
-                );
+            Graphics.Blit(null, _bakeMaterial, 0);
+
+            // Frame number update
+            _lastFrame = frame;
         }
 
         #endregion
